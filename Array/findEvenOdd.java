b9243833e71a94aa002f6ae24dd456e967592f37
Raushan-kumar88 <raushan.20jics088@jietjodhpur.ac.in>
2022-09-25 23:25:50 +0530

    modified file

--- Array/findEvenOdd.java
+++ Array/findEvenOdd.java
@@ -15,14 +15,19 @@ class findEvenOdd{
                 d2.add(arr[i]);
             }
         }
+        int sum=0;
+        int sum1=0;
         System.out.print("even element :- ");
         for(int i : d1){
             System.out.print(i+" ");
+            sum=sum+i;
         }
-        
+        System.out.println("\nthe sum of even no: "+sum);
         System.out.print("\nodd element :- ");
         for(int j : d2){
             System.out.print(j+" ");
+            sum1+=j;
         }
+        System.out.println("\nthe sum of odd no: "+sum1);
     }
 }
