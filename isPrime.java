10c1ac3934229adbe219a2784c809121dcdf1b4f
Raushan-kumar88 <raushan.20jics088@jietjodhpur.ac.in>
2022-10-16 16:59:31 +0530

    modified isPrime

--- isPrime.java
+++ isPrime.java
@@ -1,25 +1,50 @@
 // WAP to find prime of not
 import java.util.*;
 class isPrime{
-    static Boolean isPrime1(int n){
-        if(n<=1){
-            return false;
+
+    // here is prime no code......
+
+    // static Boolean isPrime1(int n){
+    //     if(n<=1){
+    //         return false;
+    //     }
+    //     int c=2;
+    //     while(c*c<=n){
+    //         if(n%c==0){
+    //             return false;
+    //         }
+    //         c++;
+    //     }
+    //     return c*c>=n;
+    // }
+
+    // here is isArmstrong no 
+    static Boolean isArmstrong(int n){
+        int original=n;
+        int result = 0;
+        int rem;
+        while(n>0){
+            rem=n%10;
+            n=n/10;
+            result+=(rem*rem*rem);
         }
-        int c=2;
-        while(c*c<=n){
-            if(n%c==0){
-                return false;
-            }
-            c++;
-        }
-        return c*c>=n;
+        return result==original;
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter any no : ");
-        int n=sc.nextInt();
-        Boolean ans=isPrime1(n);
-        System.out.println(ans);
+        // System.out.println("enter any no : ");
+        // int n=sc.nextInt();
+        
+        for(int i=1;i<1000;i++){
+            // if(isPrime1(i)){
+            //     System.out.print(i+" ");
+            // }
+            if(isArmstrong(i)){
+                System.out.print(i+" ");
+            }
+            
 
+        }
+        
     }
 }
