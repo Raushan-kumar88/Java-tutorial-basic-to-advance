cb278d779ce7525918c6575d1e3647eca9bc300a
Raushan-kumar88 <raushan.20jics088@jietjodhpur.ac.in>
2022-10-28 19:31:12 +0530

    java

--- Oop's/Polymorphism.java
+++ Oop's/Polymorphism.java
@@ -4,7 +4,37 @@
 // and Morphism means forms
 // ex:- water :- solid , liquid, ice;
 //     shape :- cone , triangle , circle;
-There are two types of Polymorphism
-compile time Polymorphism
- ->
-and run time Polymorphism
+// There are two types of Polymorphism
+// compile time Polymorphism
+//  ->it is also known as overloding.
+//  overloding :-
+//  same class
+//  same name
+//  different argument
+// and run time Polymorphism
+// -> it is alos know as overriding.
+// overriding :-
+// -> same name 
+// -> different class
+class Polymorphism{
+    static int a=0,b=1;
+    void show(int id, String name){
+        System.out.println("first method");
+    }
+    void show(String name){
+        System.out.println("second method");
+    }
+    fibnasic(int n){
+        if(n!=0){
+            a=a+b;
+        }
+    }
+    public static void main(String[] args){
+        
+        Polymorphism t = new Polymorphism();
+        t.show(10,"Raushan kumar");
+        System.out.println(a+" "+b+" ");
+        
+    }
+
+}
