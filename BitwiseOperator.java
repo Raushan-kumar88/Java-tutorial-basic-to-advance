f188161f22686a9848f60efaf52f1930f7715e46
Raushan-kumar88 <raushan.20jics088@jietjodhpur.ac.in>
2022-12-15 19:10:27 +0530

    program of bitwise operator

---
+++ BitwiseOperator.java
@@ -0,0 +1,17 @@
+class BitwiseOperator{
+    public static void main(String[] args) {
+        int a=9;
+        int b=10;
+        System.out.println(a|b); //bitwise operator OR
+        System.out.println(a&b); //bitwise operator AND
+        System.out.println(a^b); //bitwise Exclusive OR
+        System.out.println(a<<1); //bitwise  left
+        System.out.println(a<<2); 
+
+        System.out.println(b>>1); //bitwise  Right
+        System.out.println(b>>2);
+
+        // System.out.println(a~b);
+
+    }
+}
