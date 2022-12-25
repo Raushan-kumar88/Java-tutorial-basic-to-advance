6952cdf192dded67465812d1e889b66e5d3eb05e
Raushan-kumar88 <raushan.20jics088@jietjodhpur.ac.in>
2022-12-25 17:00:03 +0530

    add query question

--- Array/RotateArrayKthStep.java
+++ Array/RotateArrayKthStep.java
@@ -20,6 +20,20 @@ class RotateArrayKthStep{
         Swap(arr,0,n-1);
         // return;
     }
+
+
+    // next question funcion
+    static int[] findFequencyArray(int arr[]){
+        // here create fequency array
+        int feqArray[] = new int[100005];
+        for(int i=0;i<arr.length;i++){
+            feqArray[arr[i]]++;
+        }
+        return feqArray;
+    }
+
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int arr[] = {1,2,3,4,5,6,7,8,9};
@@ -27,5 +41,28 @@ class RotateArrayKthStep{
         int el=sc.nextInt();
         rotateArray(arr,el);
         System.out.println("your work is process ..."+Arrays.toString(arr));
+
+System.out.println("------------------------------ next question ------------------------");
+        // check no is present or not nested query 
+        int arr1[]={2,1,4,3,5,6};
+        System.out.println("enter the query of size :");
+        int kq=sc.nextInt();
+        int freq[]=findFequencyArray(arr1);
+        System.out.println("the current Array : "+Arrays.toString(arr1));
+
+        while(kq>0){
+            System.out.println("enter the query :");
+            int q=sc.nextInt();
+            if(freq[q]>0){
+            System.out.println("YES ");
+                
+            }
+            else{
+            System.out.println("NO");
+            }
+            kq--;
+        }
+
+
     }
 }
